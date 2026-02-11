640c6be8a51f7595574679dd4dfe1bc88ec78ccd
dory-finn <dorystudio.lab@gmail.com>
2026-02-11 20:28:24 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,17 @@
+# shadcn/ui
+
+一套精美设计的组件集，支持自定义、扩展与构建。你可以从这里开始，然后打造属于你自己的组件库。开源、开放代码。用它来构建你自己的组件库。
+
+![hero](apps/v4/public/opengraph-image.png)
+
+## Documentation
+
+访问 [https://www.shadcn.com.cn/docs](https://www.shadcn.com.cn/docs) 来查看中文文档
+
+## Contributing
+
+请阅读 贡献指南 [贡献指南](/CONTRIBUTING.md).
+
+## License
+
+Licensed under the [MIT license](https://github.com/shadcn/ui/blob/main/LICENSE.md).
---
+++ apps/v4/lib/config.ts
@@ -0,0 +1,50 @@
+export const siteConfig = {
+  name: "shadcn/ui 中文站",
+  url: "https://www.shadcn.com.cn",
+  ogImage: "https://www.shadcn.com.cn/og.jpg",
+  description:
+    "一组精心设计的组件，你可以自定义、扩展并在此基础上构建。从这里开始，让它成为你的作品。开源，开放代码。",
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn-ui/ui",
+  },
+  navItems: [
+    {
+      href: "/docs/installation",
+      label: "文档",
+    },
+    {
+      href: "/docs/components",
+      label: "组件",
+    },
+    {
+      href: "/blocks",
+      label: "Blocks",
+    },
+    {
+      href: "/charts/area",
+      label: "图表",
+    },
+    {
+      href: "/docs/directory",
+      label: "目录",
+    },
+    {
+      href: "/themes",
+      label: "主题",
+    },
+    {
+      href: "/colors",
+      label: "配色",
+    },
+    {
+      href: "https://www.shadcn.com.cn/dashboard",
+      label: "Dashboard",
+    },
+  ],
+}
+
+export const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#09090b",
+}
